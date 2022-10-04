fdc2d016e8bbe7de40d72a846890b374b12d6d34
Mareike <mareike.brandt@hotmail.de>
2022-10-04 22:03:35 +0900

    debug: fixed side scrolling error on mobile view

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const burger = document.querySelector("nav svg");
 
 burger.addEventListener("click", () => {
   if (burger.classList.contains("active")) {
-    gsap.to(".links", { x: "100%", duration: 0.5 });
-    gsap.to(".line", { stroke: "white", duration: 0.5 });
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
 
-    gsap.set("body", { overflow: "hidden" });
+    gsap.set("body", { overflowX: "hidden" });
   } else {
-    gsap.to(".links", { x: 0, duration: 0.5 });
-    gsap.to(".line", { stroke: "black", duration: 0.5 });
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
     gsap.fromTo(
       ".links a",
       { opacity: 0, y: 0 },
@@ -19,7 +19,7 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
 });
 
-const videos = gsap.utils.toArray("video");
+const videos = gsap.utils.toArray(".video");
 gsap.set(videos, { opacity: 0 });
 
 videos.forEach((video) => {
@@ -27,19 +27,13 @@ videos.forEach((video) => {
     trigger: video,
     start: "top center",
     end: "bottom center",
-    markers: false,
+
     onEnter: () => {
       gsap.to(video, { opacity: 1 });
       video.play();
     },
-    onEnterBack: () => {
-      video.play();
-    },
-    onLeave: () => {
-      video.pause();
-    },
-    onLeaveBack: () => {
-      video.pause();
-    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
   });
 });
